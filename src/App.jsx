29ac84032a2694583c3253083c0b2890e758cded
Frank Scott <33237633+Ziorks@users.noreply.github.com>
2025-07-27 00:07:44 -0500

    refactor site navigation to it's own component and app now stores user on load

--- src/App.jsx
+++ src/App.jsx
@@ -1,29 +1,38 @@
-import { useState } from "react";
-import { Link, Outlet, useNavigate } from "react-router-dom";
+import { useEffect, useState } from "react";
+import { Outlet, useNavigate } from "react-router-dom";
 import axios from "axios";
+import SiteNavigation from "./components/SiteNavigation";
 const host = import.meta.env.VITE_API_HOST;
 
 function App() {
+  const [user, setUser] = useState(null);
   const [isLoading, setIsLoading] = useState(false);
   const [error, setError] = useState(null);
   const navigate = useNavigate();
 
-  const handleLogout = () => {
+  useEffect(() => {
+    let ignore = false;
     setIsLoading(true);
     setError(null);
 
     axios
-      .get(`${host}/logout`, {
+      .get(`${host}/user/me`, {
         withCredentials: true,
       })
-      .then(() => {
-        navigate("/login");
+      .then((resp) => {
+        if (ignore) return;
+        setUser(resp.data.user);
       })
       .catch((err) => {
+        if (ignore) return;
         console.log(err);
 
         if (err.response) {
-          setError(err.response.data.error);
+          if (err.response.status === 401) {
+            navigate("/login");
+          } else {
+            setError(err.response.data.error);
+          }
         } else {
           setError("Something went wrong. Please try again.");
         }
@@ -31,12 +40,13 @@ function App() {
       .finally(() => {
         setIsLoading(false);
       });
-  };
+
+    return () => (ignore = true);
+  }, [navigate]);
+
   return (
     <>
-      <Link to={"/"}>Home</Link> <Link to={"/users"}>Users</Link>{" "}
-      <Link to={"/conversations"}>Conversations</Link>{" "}
-      <button onClick={handleLogout}>Logout</button>
+      <SiteNavigation user={user} />
       <Outlet />
     </>
   );
---
+++ src/components/SiteNavigation/SiteNavigation.jsx
@@ -0,0 +1,54 @@
+import { useState } from "react";
+import { Link, useNavigate } from "react-router-dom";
+import axios from "axios";
+const host = import.meta.env.VITE_API_HOST;
+// import styles from "./SiteNavigation.module.css";
+
+function SiteNavigation({ user }) {
+  //TODO: show the users profile picture and username
+  //TODO: if user===null show placeholder OR don't render this until the user has loaded (less desirable)
+
+  const [isLoading, setIsLoading] = useState(false);
+  const [error, setError] = useState(null);
+  const navigate = useNavigate();
+
+  const handleLogout = () => {
+    setIsLoading(true);
+    setError(null);
+
+    axios
+      .get(`${host}/logout`, {
+        withCredentials: true,
+      })
+      .then(() => {
+        navigate("/login");
+      })
+      .catch((err) => {
+        console.log(err);
+
+        if (err.response) {
+          setError(err.response.data.error);
+        } else {
+          setError("Something went wrong. Please try again.");
+        }
+      })
+      .finally(() => {
+        setIsLoading(false);
+      });
+  };
+
+  return (
+    <>
+      {user && (
+        <nav>
+          <Link to={"/"}>Home</Link> <Link to={"/users"}>Users</Link>{" "}
+          <Link to={"/conversations"}>Conversations</Link>{" "}
+          <span>Welcome back, {user.username}</span>{" "}
+          <button onClick={handleLogout}>Logout</button>
+        </nav>
+      )}
+    </>
+  );
+}
+
+export default SiteNavigation;
